64b4a04fd7c16cd73f4fa53112fe95cdaee3538a
Tolga Sönmez <sonmeztolga58@gmail.com>
2025-01-05 00:29:03 +0300

    Entity Create
    Creating entities and organizing their internal structures.

---
+++ Core/hepsiburada.domain/Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿using hepsiburada.domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hepsiburada.domain.Entities
+{
+    public class Product : EntityBase, IEntityBase
+    {
+        public Product() { }    
+        public Product(string title, string description, int brandId, decimal price, decimal discount)
+        {
+            Title = title;
+            Description = description;
+            BrandId = brandId;
+            Price = price;
+            Discount = discount;
+        }
+
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required int BrandId { get; set; }
+        public required decimal Price { get; set; }
+        public required decimal Discount { get; set; }
+        
+        public Brand Brand { get; set; }
+
+        public ICollection<Category> Categories{ get; set; }
+    }
+}
