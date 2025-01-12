7f382a5c4253783f2829fcfbdb0d7741bb10e964
Tolga Sönmez <sonmeztolga58@gmail.com>
2025-01-12 06:47:08 +0300

    BaseRules Create
    Introduced BaseRules structure to standardize and simplify business rule validations.

---
+++ Core/hepsiburada.app/Features/Products/Exceptions/ProductTitleMustNotBeSameException.cs
@@ -0,0 +1,17 @@
+﻿using hepsiburada.app.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hepsiburada.app.Features.Products.Exceptions
+{
+    public class ProductTitleMustNotBeSameException : BaseExceptions
+    {
+        public ProductTitleMustNotBeSameException() : base("Product title must not be same.")
+        {
+
+        }
+    }
+}
---
+++ Core/hepsiburada.app/Features/Products/Rules/ProductRules.cs
@@ -0,0 +1,23 @@
+﻿using hepsiburada.app.Bases;
+using hepsiburada.app.Features.Products.Exceptions;
+using hepsiburada.domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hepsiburada.app.Features.Products.Rules
+{
+    public class ProductRules : BaseRules
+    {
+        public Task ProductTitleMustNotBeSame(IList<Product> products, string requestTitle)
+        {
+            if(products.Any(x=>x.Title == requestTitle))
+            {
+                throw new ProductTitleMustNotBeSameException();
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
