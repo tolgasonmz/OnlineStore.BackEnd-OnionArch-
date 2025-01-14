ac8c75f3bc091983195da18cc259295dbac5e1e0
Tolga Sönmez <sonmeztolga58@gmail.com>
2025-01-14 05:37:06 +0300

    Token Service & JWT
    Configured JWT and implemented the foundation for Token Service.

---
+++ Infrastructure/hepsiburada.Infrastructure/Tokens/TokenSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hepsiburada.Infrastructure.Tokens
+{
+    public class TokenSettings
+    {
+        public string Secret { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int TokenValidityMunitues { get; set; }
+    }
+}
--- Presantation/hepsiburada.Api/Program.cs
+++ Presantation/hepsiburada.Api/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using hepsiburada.app;
 using hepsiburada.Mapper;
 using hepsiburada.app.Exceotions;
+using hepsiburada.Infrastructure;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +14,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var env = builder.Environment;
 
@@ -26,8 +27,38 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApp();
 builder.Services.AddCustomMapper();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "hepsiburada.Api", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme." //exam: Bearer nimc!q(e9cg$t9!mxz*=9myz0z-_5_3knr&&^4)4gf)e5xz6va
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+
+    });
+});
 
 var app = builder.Build();
 
