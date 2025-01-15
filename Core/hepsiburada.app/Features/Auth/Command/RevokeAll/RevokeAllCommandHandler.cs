b4319c071a099dd1c9fb307f7cf5f81754036d67
Tolga Sönmez <sonmeztolga58@gmail.com>
2025-01-15 04:02:16 +0300

    Jwt Logout
    Implemented JWT Logout with a basic revoke token functionality

---
+++ Core/hepsiburada.app/Features/Auth/Command/RevokeAll/RevokeAllCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using hepsiburada.app.Bases;
+using hepsiburada.app.Interfaces.AutoMapper;
+using hepsiburada.app.Interfaces.UnitOfWorks;
+using hepsiburada.domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hepsiburada.app.Features.Auth.Command.RevokeAll
+{
+    public class RevokeAllCommandHandler : BaseHandler, IRequestHandler<RevokeAllCommandRequest, Unit>
+    {
+        private readonly UserManager<User> userManager;
+        public RevokeAllCommandHandler(UserManager<User> userManager, IUnitOfWork unitOfWork, IMapper mapper, IHttpContextAccessor httpContextAccessor) : base(unitOfWork, mapper, httpContextAccessor)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<Unit> Handle(RevokeAllCommandRequest request, CancellationToken cancellationToken)
+        {
+            var users = await userManager.Users.ToListAsync();
+            foreach (var user in users)
+            {
+                user.RefreshToken = null;
+                await userManager.UpdateAsync(user);
+            }
+            return Unit.Value;
+        }
+    }
+}
