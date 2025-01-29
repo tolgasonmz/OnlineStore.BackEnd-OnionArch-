19e7e3d69cf7483f2f49c7c5296e1a382206e10f
Tolga Sönmez <sonmeztolga58@gmail.com>
2025-01-29 05:44:47 +0300

    Brand Features Completion and Validation Enhancements
    -Added missing Update and Delete functionalities to Brand features.
    -Implemented validators for Create and other related fields to ensure data integrity.

---
+++ Core/OnlineStore.app/Features/Brands/Commands/UpdateBrand/UpdateBrandCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.app.Features.Brands.Commands.UpdateBrand
+{
+    public class UpdateBrandCommandValidator : AbstractValidator<UpdateBrandCommandRequest>
+    {
+        public UpdateBrandCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Name is required");
+        }
+    }
+}
--- Presantation/OnlineStore.Api/Controllers/BrandController.cs
+++ Presantation/OnlineStore.Api/Controllers/BrandController.cs
@@ -4,6 +4,8 @@ using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using OnlineStore.app.Features.Brands.Commands.UpdateBrand;
+using OnlineStore.app.Features.Brands.Commands.DeleteBrand;
 
 namespace OnlineStore.Api.Controllers
 {
@@ -25,6 +27,18 @@ namespace OnlineStore.Api.Controllers
 
         [HttpPost]
         public async Task<IActionResult> CreateBrand(CreateBrandCommandRequest request)
+        {
+            await mediator.Send(request);
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateBrand(UpdateBrandCommandRequest request)
+        {
+            await mediator.Send(request);
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteBrand(DeleteBrandCommandRequest request)
         {
             await mediator.Send(request);
             return Ok();
